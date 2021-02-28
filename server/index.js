7a96c58367b5d924112f171e10234cfac80a1231
Jacob Fijas <jacobfij@gatech.edu>
2021-02-28 15:57:48 -0600

    update startup statement to indicate correct port

--- server/index.js
+++ server/index.js
@@ -32,4 +32,4 @@ const server = http.createServer(app)
 const io = require('socket.io')(server)
 
 const PORT = process.env.PORT || 3000
-server.listen(PORT, () => console.log('server started on port 5000'))
+server.listen(PORT, () => console.log(`server started on port ${PORT}`))
